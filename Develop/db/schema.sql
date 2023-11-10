f81f00bb039bcc189fbc8387d1e2885f5f65edde
arunp44 <137665388+arunp44@users.noreply.github.com>
2023-11-09 22:56:34 -0700

    test

---
+++ Develop/db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS company;
+CREATE DATABASE company;
+
+CREATE TABLE department (
+    id int NOT NULL,
+    name varchar(30) NOT NULL,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE role (
+    id int NOT NULL,
+    titel varchar(30),
+    salery decimal,
+    department_id int NOT NULL,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Employee (
+    id int NOT NULL,
+    LastName varchar(30) NOT NULL,
+    FirstName varchar(30) NOT NULL,
+    roleID int,
+    managerID int,
+    PRIMARY KEY (ID)
+);
